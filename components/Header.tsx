482a2605088b7cf29f445a62c78b249758a5ee75
mwyywm <hasanof.rasmus@gmail.com>
2022-05-03 22:13:26 +0200

    tailwind added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,69 +1,12 @@
-import React from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from "react";
 
 const Header: React.FC = () => {
-  const router = useRouter()
-  const isActive: (pathname: string) => boolean =
-    pathname => router.pathname === pathname
-
-  return(
-    <nav>
-      <div className="left">
-        <Link href="/">
-          <a className="bold" data-active={isActive('/')}>
-            Blog
-          </a>
-        </Link>
-        <Link href="/drafts">
-          <a data-active={isActive('/drafts')}>Drafts</a>
-        </Link>
-      </div>
-      <div className="right">
-        <Link href="/signup">
-          <a data-active={isActive('/signup')}>Signup</a>
-        </Link>
-        <Link href="/create">
-          <a data-active={isActive('/create')}>+ Create draft</a>
-        </Link>
-      </div>
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-
-        .bold {
-          font-weight: bold;
-        }
-
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
-
-        .left a[data-active='true'] {
-          color: gray;
-        }
-
-        a + a {
-          margin-left: 1rem;
-        }
-
-        .right {
-          margin-left: auto;
-        }
-
-        .right a {
-          border: 1px solid black;
-          padding: 0.5rem 1rem;
-          border-radius: 3px;
-        }
-      `}</style>
+  return (
+    <nav className="w-full">
+      <h1>Quiz</h1>
+      <p>This is the header</p>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,13 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
 
-// POST /api/user
-// Required fields in body: name, email
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const result = await prisma.user.create({
     data: {
       ...req.body,
     },
-  })
-  res.json(result)
+  });
+  res.json(result);
 }
