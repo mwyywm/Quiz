570af65b4e67fc7a0e3590e5ba210e5f692b9e7f
mwyywm <hasanof.rasmus@gmail.com>
2022-05-06 04:11:45 +0200

    Changes to newquiz handler and fixed build error

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -1,7 +1,3 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-
 export default function QuizForm() {
-  const { register, handleSubmit, errors } = useForm();
   return <></>;
 }
--- pages/api/newquiz.ts
+++ pages/api/newquiz.ts
@@ -7,6 +7,23 @@ export default async function quizHandler(
   res: NextApiResponse
 ) {
   const body = req.body;
+  let arr = [
+    {
+      question: "Random question 1",
+      correctAnswer: "right",
+      answers: ["11231223", "4535236"],
+    },
+    {
+      question: "Random question 2",
+      correctAnswer: "123",
+      answers: ["163623", "456", "hi"],
+    },
+    {
+      question: "Random question 3",
+      correctAnswer: "321",
+      answers: ["1211233", "4513136"],
+    },
+  ];
   // title, description, questions (array of questions)
   // questions: { question, answers (array of answers), correctAnswer }
   if (req.method === "POST") {
@@ -16,19 +33,15 @@ export default async function quizHandler(
           title: body.title,
           description: body.description,
           questions: {
-            create: {
-              question: "what is the meaning of life?",
-              correctAnswer: "ras",
-              answers: ["ras", "test", "mus"],
-              // need to programmatically generate answers
-              // and "question"
+            createMany: {
+              data: arr,
             },
           },
         },
       });
       return res.json(result);
-    } catch (e) {
-      return res.status(500).json({ message: e.message });
+    } catch (error: any) {
+      return res.status(500).json({ message: error.message });
     }
   } else {
     return res.status(405).end("Only POST requests are allowed");
