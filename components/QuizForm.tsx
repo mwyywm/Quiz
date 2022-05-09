e38ef06b9a75b03cea3d62c0f50319aff040267f
mwyywm <hasanof.rasmus@gmail.com>
2022-05-09 17:48:36 +0200

    initial form

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -1,3 +1,67 @@
+// react hook form import
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+
 export default function QuizForm() {
-  return <></>;
+  const { register, control, handleSubmit, reset, watch } = useForm({});
+  const { fields, append, prepend, remove, swap, move, insert, replace } =
+    useFieldArray({
+      control,
+      name: "questions",
+    });
+  const onSubmit = (data: any) => {
+    console.log(data);
+    setData(data);
+  };
+  const [data, setData] = useState(null);
+
+  return (
+    <>
+      <h1>QuizForm</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col text-black"
+      >
+        <input
+          placeholder="quiztitle"
+          {...register("title", { required: true })}
+        />
+        <input
+          placeholder="description"
+          {...register("description", { required: true })}
+        />
+        {fields.map((field: any, index: number) => (
+          <React.Fragment key={index}>
+            <input
+              placeholder={`questions[${index}].question[${JSON.stringify(
+                field.id
+              )}]}`}
+              {...register(`questions[${index}].question`, {})}
+            />
+            {["", ""].map((answer: string, i: number) => (
+              <input
+                placeholder={`questions[${index}].answer[${JSON.stringify(
+                  field.id
+                )}]`}
+                {...register(`questions[${index}].answer[${i}]`, {})}
+              />
+            ))}
+            <button className="text-white" onClick={() => remove(index)}>
+              Remove
+            </button>
+          </React.Fragment>
+        ))}
+        <button
+          className="text-white"
+          onClick={() => {
+            append({});
+          }}
+        >
+          append
+        </button>
+        <input className="text-white" type="submit" />
+      </form>
+      <code>{JSON.stringify(data)}</code>
+    </>
+  );
 }
