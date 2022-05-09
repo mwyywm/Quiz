b181b7b1c7b7411493492437da9f6f52decf10b3
mwyywm <hasanof.rasmus@gmail.com>
2022-05-09 15:27:58 +0200

    Added slug to prisma model & creating slug out of title on new quiz creation

--- pages/api/newquiz.ts
+++ pages/api/newquiz.ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
+import slugify from "../../utils/slugify";
 
 // create a new quiz
 export default async function quizHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // title, description, questions (array of objects)
   const body = req.body;
+  // questions: { question, answers (array of answers), correctAnswer }
   let arr = [
     {
       question: "Random question 1",
@@ -24,17 +27,16 @@ export default async function quizHandler(
       answers: ["1211233", "4513136"],
     },
   ];
-  // title, description, questions (array of questions)
-  // questions: { question, answers (array of answers), correctAnswer }
   if (req.method === "POST") {
     try {
       const result = await prisma.quiz.create({
         data: {
           title: body.title,
+          slug: slugify(body.title),
           description: body.description,
           questions: {
             createMany: {
-              data: arr,
+              data: arr, // array of objects holding questions{question, correctAnswer, answers (array of answers)}
             },
           },
         },
