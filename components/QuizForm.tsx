313a7448cae3a9330bc6c88e321ef294766055d9
mwyywm <hasanof.rasmus@gmail.com>
2022-05-10 20:41:27 +0200

    form post request working with fetch

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -1,5 +1,5 @@
 // react hook form import
-import { useForm, useFieldArray, appendErrors } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import React, { useState, useEffect } from "react";
 import { DevTool } from "@hookform/devtools";
 import clsx from "clsx";
@@ -9,17 +9,51 @@ interface answerAmountType {
 }
 
 export default function App() {
-  const { register, control, handleSubmit, watch, setFocus } = useForm({});
+  const { register, control, handleSubmit, setFocus, formState, getValues } =
+    useForm({});
+  const { isDirty, isSubmitting, isSubmitSuccessful, isValid } = formState;
   const { fields, append, remove } = useFieldArray({
     control,
     name: "questions",
   });
+  // in answerAmount we have an array for every field.id we have in the form.
+  // answerAmount[field.id] holds an array of undefined values. The length of this array is the amount of answers for that field.
   const [answerAmount, setAnswerAmount] = useState<answerAmountType>({
     init: [undefined, undefined],
   });
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({});
   const onSubmit = (data: any) => {
-    setData(data);
+    // looping over the questions array and adding the correctAnswer as a string
+    console.log(data);
+    const formattedQuestions = data.questions.map((obj: any, i: number) => {
+      return {
+        ...obj,
+        correctAnswer: data.questions[i].answers[Number(obj.correctAnswer)],
+      };
+    });
+    setData({
+      title: data.title,
+      description: data.description,
+      questions: formattedQuestions,
+    });
+
+    fetch("/api/newquiz", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: data.title,
+        description: data.description,
+        questions: formattedQuestions,
+      }),
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   const handleNewQuestion = (id: string) => {
     const AddQuestion = (arrLen: number): number | undefined => {
@@ -55,20 +89,29 @@ export default function App() {
 
   return (
     <>
+      <DevTool control={control} />
       <h1 className="mt-4 text-lg text-white">QuizForm</h1>
       <form
         className="flex w-full flex-col text-black"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <DevTool control={control} />
         <input
           placeholder="title"
-          {...register("title", { required: true })}
+          {...register("title", {
+            required: true,
+            maxLength: 80,
+            minLength: 3,
+            pattern: /^[a-zA-Z-0-9 ]*$/,
+          })}
           className="mb-2 h-10 w-full pl-1"
         />
         <input
           placeholder="description"
-          {...register("description", { required: true })}
+          {...register("description", {
+            required: "Description required",
+            minLength: 3,
+            maxLength: 160,
+          })}
           className="h-10 w-full pl-1"
         />
         {fields.map((field: any, index: number) => (
@@ -86,14 +129,14 @@ export default function App() {
                 <div className="flex items-center justify-center" key={i}>
                   <input
                     placeholder={`answer ${i + 1}`}
-                    {...register(`questions[${index}].answer[${i}]`, {
+                    {...register(`questions[${index}].answers[${i}]`, {
                       required: true,
                     })}
                     className="my-1 h-10 w-full pl-1"
                   />
                   <input
                     type="radio"
-                    value={watch(`questions[${index}].answer[${i}]`)}
+                    value={i}
                     {...register(`questions[${index}].correctAnswer`, {
                       required: true,
                     })}
@@ -105,12 +148,14 @@ export default function App() {
               {!(answerAmount[field.id]?.length === 4) && (
                 <input
                   value="Add answer"
+                  readOnly
                   className="my-1 mr-1 w-full cursor-pointer rounded-sm bg-amber-500 px-4 py-2 text-center text-black transition-colors hover:bg-[#ffad21]"
                   onClick={() => handleNewQuestion(field.id)}
                 />
               )}
               <input
                 value={`Remove question ${index + 1}`}
+                readOnly
                 className={clsx(
                   "my-1 ml-1 w-full cursor-pointer rounded-sm bg-amber-500 px-4 py-2 text-center text-black transition-colors hover:bg-[#ffad21]",
                   {
@@ -124,13 +169,14 @@ export default function App() {
         ))}
         <input
           value="Add question"
+          readOnly
           className="my-1 cursor-pointer rounded-sm bg-amber-500 px-4 py-2 text-center text-black transition-colors hover:bg-[#ffad21]"
           onClick={() => {
-            append({ question: "", answer: [] });
+            append({ question: "", answers: [] });
           }}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              append({ question: "", answer: [] });
+              append({ question: "", answers: [] });
             }
           }}
         />
