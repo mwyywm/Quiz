f1338db80fe99050d4bd5102561abbae7e0428bc
mwyywm <hasanof.rasmus@gmail.com>
2022-05-25 15:11:39 +0200

    removed correctAnswer from query & added proper types

--- pages/quiz/[index].tsx
+++ pages/quiz/[index].tsx
@@ -3,12 +3,25 @@ import { GetServerSideProps } from "next";
 import prisma from "../../lib/prisma";
 
 export interface Props {
-  params: string;
+  quiz: QuizType;
+}
+interface QuizType {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  questions: {
+    id: number;
+    question: string;
+    answers: string[];
+    correctAnswer?: string;
+  };
 }
 
 const Quiz = ({ quiz }: Props) => {
   return (
     <Layout>
+      {console.log(quiz)}
       <div className="mx-auto w-[650px] max-w-full">
         <h1 className="mb-4 text-center text-5xl font-bold antialiased">
           {quiz.title}
@@ -16,24 +29,8 @@ const Quiz = ({ quiz }: Props) => {
         <p className="mx-auto mt-5 w-[550px] max-w-full text-center text-xl font-normal text-gray-300 antialiased">
           {quiz.description}
         </p>
-        <div>
-          <ul className="list-inside list-disc">
-            {quiz.questions.map((question) => (
-              <li key={question.id}>
-                <h2 className="text-xl font-bold antialiased">
-                  {question.question}
-                </h2>
-                <ul className="list-inside list-disc">
-                  {question.answers.map((answer) => (
-                    <li key={answer.id}>{answer.answer}</li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <div>{/* quiz taking component goes here */}</div>
       </div>
-      {console.log(quiz)}
     </Layout>
   );
 };
@@ -45,7 +42,14 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       slug: index && index.toString(),
     },
     include: {
-      questions: true,
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          answers: true,
+          correctAnswer: false,
+        },
+      },
     },
   });
 
