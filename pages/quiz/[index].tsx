ff832f5ac0c184a9ee6f793e7aed61399a015ff4
mwyywm <hasanof.rasmus@gmail.com>
2022-06-08 14:59:21 +0200

    initial taking quiz component

--- pages/quiz/[index].tsx
+++ pages/quiz/[index].tsx
@@ -1,8 +1,7 @@
 import Layout from "../../components/Layout";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import prisma from "../../lib/prisma";
-import clsx from "clsx";
 
 export interface Props {
   quiz: QuizType;
@@ -20,7 +19,6 @@ interface QuestionType {
   answers: string[];
   correctAnswer?: string;
 }
-
 interface AnswerState {
   title: string;
   slug: string;
@@ -30,28 +28,26 @@ interface AnswerState {
   };
 }
 const Quiz = ({ quiz }: Props) => {
-  const [answersObj, setAnswersObj] = useState<AnswerState>({} as AnswerState);
-  const handleChange = (
-    e: React.ChangeEvent<HTMLSelectElement>,
-    question: QuestionType
-  ) => {
-    setAnswersObj({
-      ...answersObj,
-      questions: {
-        ...answersObj.questions,
-        [question.id]: e.target.value,
-      },
-    });
-    console.log("answersObj", answersObj);
+  const [answersObj, setAnswersObj] = useState<AnswerState>({
+    title: quiz.title,
+    slug: quiz.slug,
+    quizID: quiz.id,
+    questions: {},
+  } as AnswerState);
+  const [currentQuestionID, setCurrentQuestionID] = useState(0);
+  // the total amount of questions should be quiz.questions.length - 1.
+  console.log(quiz.questions[currentQuestionID]);
+  const handleNextQuestion = () => {
+    if (currentQuestionID < quiz.questions.length - 1) {
+      setCurrentQuestionID(currentQuestionID + 1);
+    } else {
+      // handle submit
+    }
   };
-  useEffect(() => {
-    setAnswersObj({
-      title: quiz.title,
-      slug: quiz.slug,
-      quizID: quiz.id,
-      questions: {},
-    });
-  }, []);
+  const handleAnswer = (answer: string) => {
+    // handle answer
+    console.log(answer);
+  };
   return (
     <Layout>
       <div className="mx-auto w-[650px] max-w-full">
@@ -61,34 +57,28 @@ const Quiz = ({ quiz }: Props) => {
         <p className="mx-auto mt-5 w-[550px] max-w-full text-center text-xl font-normal text-gray-300 antialiased">
           {quiz.description}
         </p>
-        <div>
-          <ul>
-            {quiz.questions.map((question: QuestionType) => (
-              <li key={question.id}>
-                <p className="text-xl font-bold antialiased">
-                  {question.question}
-                </p>
-                <select
-                  className="text-black"
-                  onChange={(e) => handleChange(e, question)}
-                >
-                  {question.answers.map((answer: string) => (
-                    <option className="text-black" key={answer}>
-                      {answer}
-                    </option>
-                  ))}
-                </select>
-              </li>
+        {/* the current question prev/next component below */}
+        <div className="mt-5 flex">
+          <h2 className="text-left">
+            {quiz.questions[currentQuestionID]?.question}
+          </h2>
+          <div>
+            {quiz.questions[currentQuestionID]?.answers.map((answer) => (
+              <button
+                className="mx-4 bg-gray-600 p-2"
+                onClick={() => handleAnswer(answer)}
+              >
+                {answer}
+              </button>
             ))}
-          </ul>
+          </div>
         </div>
+        {/* the current question prev/next component above */}
         <button
-          className="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-          onClick={() => {
-            console.log(answersObj);
-          }}
+          className="rounded-md bg-blue-400 p-2 font-semibold text-black"
+          onClick={handleNextQuestion}
         >
-          send
+          next
         </button>
       </div>
     </Layout>
