13b559f41a698e3bc2d98f5634b8af4046cb22aa
mwyywm <hasanof.rasmus@gmail.com>
2022-06-14 13:52:31 +0200

    showing results for user

--- pages/quiz/[slug]/results/index.tsx
+++ pages/quiz/[slug]/results/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../../../../components/Layout";
 
 interface Props {
   quizResult: QuizResultType;
-  user: ResultsType["username"];
+  user: ResultsType;
 }
 export interface QuizResultType {
   id: number;
@@ -18,23 +18,37 @@ export interface ResultsType {
 }
 
 const QuizResults = ({ quizResult, user }: Props) => {
+  // if the user is true we show the result of the latest quiz made by the ?user= and the leaderboard.
+  // if the user is falsy we just show the leaderboard.
   console.log("username", user);
   return (
     <Layout>
-      <h1>{quizResult.title}</h1>
-      {user && (
-        <div>
-          <h2>{JSON.stringify(user)}</h2>
-        </div>
-      )}
-      <ul>
-        {quizResult.results.map((result) => (
-          <li key={result.id}>
-            {result.username} - {result.score * 100} / {result.total * 100}{" "}
-            points
-          </li>
-        ))}
-      </ul>
+      <div className="mx-auto w-[650px] max-w-full">
+        <h1 className="mb-4 break-words text-center text-[40px] font-bold antialiased">
+          {quizResult.title}
+        </h1>
+        {user && (
+          <div>
+            <h2 className="mb-4 break-words text-center text-3xl font-normal antialiased">
+              Your score
+            </h2>
+            <div className="flex max-w-full items-center justify-between bg-white p-2 py-4 text-black">
+              <p className="w-[75%] break-words text-xl">{user.username}</p>
+              <p className="max-w-[25%] break-words text-xl">
+                {user.score * 100} points
+              </p>
+            </div>
+          </div>
+        )}
+        <ul>
+          {quizResult.results.map((result) => (
+            <li key={result.id}>
+              {result.username} - {result.score * 100} / {result.total * 100}{" "}
+              points
+            </li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
 };
