2b0f13ad68da37fed2bc9db6475df88f42551bb4
mwyywm <hasanof.rasmus@gmail.com>
2022-06-15 17:23:38 +0200

    QuizProgress: smooth transition for progress %
    Now the percentage transitions smoothly along with the progress bar.

--- components/QuizProgress.tsx
+++ components/QuizProgress.tsx
@@ -1,16 +1,35 @@
+import { useSpring, animated, config } from "@react-spring/web";
 interface Props {
   current: number;
   total: number;
 }
 const QuizProgress = ({ current, total }: Props) => {
   const progressPercentage = (current / total) * 100;
+  const previusProgressPercentage = (current - 1 / total) * 100;
+  console.log("curr", current - 1 / total);
+  const numberSpring = useSpring({
+    number: progressPercentage
+      ? progressPercentage
+      : previusProgressPercentage || 0,
+    config: {
+      duration: 500,
+      bezier: [0.42, 0, 0.58, 1.0], // equivalent to ease-in-out
+    },
+  });
+
   return (
     <>
       <div className="mb-1 flex justify-between">
         <p className="text-lg font-normal">
           Question {current}/{total}
         </p>
-        <p className="text-lg font-normal">{(current / total) * 100}%</p>
+        <p className="text-lg font-normal">
+          <animated.span>
+            {numberSpring.number.to((x) => x.toFixed(0))}
+          </animated.span>
+          %
+        </p>
+        {/* <p className="text-lg font-normal">{(current / total) * 100}%</p> */}
       </div>
       <div className="mb-1 h-4 w-full rounded-lg bg-gray-700">
         <div
