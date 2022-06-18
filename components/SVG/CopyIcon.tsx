e298f77a38553e81c765e25d57d5aacf3a38c304
mwyywm <hasanof.rasmus@gmail.com>
2022-06-18 12:15:58 +0200

    Conditionally rendering icon

---
+++ components/SVG/CopyIcon.tsx
@@ -0,0 +1,16 @@
+export default function CopyIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="black"
+        d="M14 22H4a1.934 1.934 0 01-2-2V10a1.934 1.934 0 012-2h4V4a1.934 1.934 0 012-2h10a1.934 1.934 0 012 2v10a1.935 1.935 0 01-2 2h-4v4a1.935 1.935 0 01-2 2zM4 10v10h10v-4h-4a1.935 1.935 0 01-2-2v-4H4zm6-6v10h10V4H10z"
+      ></path>
+    </svg>
+  );
+}
