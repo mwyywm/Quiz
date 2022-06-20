7a888a78331656136b9c4c65563acfbedb8dd16f
mwyywm <hasanof.rasmus@gmail.com>
2022-06-20 15:40:05 +0200

    landing: Recommended field
    full width on landing buttons when under xs width. (xs = 400px)
    Text styles on long title texts in QuizButton component.

--- components/QuizButton.tsx
+++ components/QuizButton.tsx
@@ -1,14 +1,23 @@
+import Link from "next/link";
+
 interface Props {
   title: string;
   questionsCount: number;
+  slug: string;
 }
 
-const QuizButton = ({ title, questionsCount }: Props) => {
+const QuizButton = ({ title, questionsCount, slug }: Props) => {
   return (
-    <button className="flex h-16 w-full items-center justify-between bg-amber-500 p-4 text-black transition-colors hover:bg-[#ffad21]">
-      <p className="text-xl">{title}</p>
-      <p className="text-xl">{questionsCount} questions</p>
-    </button>
+    <Link href={`/quiz/${slug}`}>
+      <a className="flex h-auto min-h-[64px] w-full flex-col items-center justify-between bg-amber-500 p-4 text-black transition-colors hover:bg-[#ffad21] xs:flex-row">
+        <p className="h-auto w-full break-words text-center text-xl font-semibold xs:w-auto xs:min-w-[60%] xs:truncate xs:pr-2 xs:text-left xs:font-normal">
+          {title}
+        </p>
+        <p className="text-center text-xl xs:min-w-fit xs:text-right">
+          {questionsCount} questions
+        </p>
+      </a>
+    </Link>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,9 +14,10 @@ interface popularQuiz {
 }
 interface Props {
   popularQuizzes: popularQuiz[];
+  recommendedQuizzes: popularQuiz[];
 }
 
-const Landing = ({ popularQuizzes }: Props) => {
+const Landing = ({ popularQuizzes, recommendedQuizzes }: Props) => {
   return (
     <Layout>
       <section className="mb-24">
@@ -25,35 +26,56 @@ const Landing = ({ popularQuizzes }: Props) => {
           <meta name="description" content="Create your own quiz!" />
         </Head>
         <div className="mx-auto w-[650px] max-w-full">
-          <h1 className="text-center text-5xl font-bold antialiased">
+          <h1 className="break-words text-center text-4xl font-bold antialiased xs:text-5xl">
             Create your own quiz!
           </h1>
-          <p className="mx-auto mt-5 w-[550px] max-w-full text-center text-xl font-normal text-gray-300 antialiased">
+          <p className="mx-auto mt-5 w-[550px] max-w-full break-words text-center text-xl font-normal text-gray-300 antialiased">
             Complete a quiz from the list below or create your own! See how you
             score compared to everyone else.
           </p>
         </div>
         <div className="mx-auto mt-16 flex flex-wrap justify-center">
-          <div className="m-2">
+          <div className="m-2 w-full xs:w-auto">
             <LinkAsButton href="/createquiz">Create quiz</LinkAsButton>
           </div>
-          <div className="m-2">
+          <div className="m-2 w-full xs:w-auto">
             <LinkAsButton href="/randomquiz" variant="secondary">
               Random quiz
             </LinkAsButton>
           </div>
         </div>
       </section>
-      <section className="min-h-[200px] ">
-        <h2 className="text-center text-4xl font-bold">Most popular</h2>
+      <section className="min-h-[200px]">
+        <h2 className="max-w-full break-words text-center text-3xl font-bold xs:text-4xl">
+          Most popular
+        </h2>
         <div className="mt-12">
           {popularQuizzes.map((quiz) => (
-            <div className="m-auto my-5 w-[650px] max-w-full" key={quiz.title}>
+            <div className="m-auto my-3 w-[650px] max-w-full" key={quiz.title}>
               <QuizButton
                 title={quiz.title}
                 questionsCount={quiz.questionsCount}
+                slug={quiz.slug}
               />
-              {console.log(quiz)}
+            </div>
+          ))}
+        </div>
+        <h3 className="mt-12 break-all text-center text-3xl font-bold xs:text-4xl">
+          Recommended
+        </h3>
+        <div className="mt-12 mb-12">
+          {/* <div className="m-auto my-5 grid w-[650px] max-w-full gap-3">
+            <QuizButton title="cool" questionsCount={12} slug="cool" />
+            <QuizButton title="fouro" questionsCount={20} slug="fouro" />
+            <QuizButton title="tenten" questionsCount={12} slug="tenten" />
+          </div> */}
+          {recommendedQuizzes.map((quiz) => (
+            <div className="m-auto my-3 w-[650px] max-w-full" key={quiz.title}>
+              <QuizButton
+                title={quiz.title}
+                questionsCount={quiz.questionsCount}
+                slug={quiz.slug}
+              />
             </div>
           ))}
         </div>
@@ -93,9 +115,16 @@ export const getStaticProps: GetStaticProps = async () => {
       };
       return returnObj;
     });
+
+  const recommended = [
+    { title: "cool", slug: "cool", questionsCount: 12 },
+    { title: "fouro", slug: "fouro", questionsCount: 20 },
+    { title: "tenten", slug: "tenten", questionsCount: 12 },
+  ];
   return {
     props: {
       popularQuizzes: sortedPopularQuizzes, // 3 quizzes with the most results
+      recommendedQuizzes: recommended, // 3 recommended quizzes
     },
     revalidate: 300,
   };
