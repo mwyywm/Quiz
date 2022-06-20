7fc9cca8557460f7fbe0055f7a683535a157838b
mwyywm <hasanof.rasmus@gmail.com>
2022-06-20 16:35:35 +0200

    Nprogress loading indicator on navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +12,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
+        <NextNProgress
+          color="#F59E0B"
+          startPosition={0.2}
+          height={2}
+          options={{ showSpinner: false }}
+        />
         <Component {...pageProps} />
       </Layout>
     </>
