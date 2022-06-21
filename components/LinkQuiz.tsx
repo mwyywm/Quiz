ce163315ad33dcf909a17ab4d2da1bf15cfeb56f
mwyywm <hasanof.rasmus@gmail.com>
2022-06-21 17:16:22 +0200

    refactor: Linkquiz component positioning
    Removed  the Tooltip component and implemented all the functionality within Tooltip into using LinkQuiz component using react-popper. The positioning now works like it should on small viewport widths.

--- components/LinkQuiz.tsx
+++ components/LinkQuiz.tsx
@@ -1,10 +1,9 @@
-import React, { useRef, useState, useLayoutEffect, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import FadeInFadeOut from "./Animation/FadeInFadeOut";
 import { CopiedObjTypes } from "./QuizCreated";
 import clsx from "clsx";
 import CheckIcon from "./SVG/CheckIcon";
 import CopyIcon from "./SVG/CopyIcon";
-import Tooltip from "./Tooltip";
 
 import { usePopper } from "react-popper";
 import Portal from "./Portal";
@@ -15,12 +14,13 @@ interface Props {
   setCopiedObj: React.Dispatch<React.SetStateAction<CopiedObjTypes>>;
   linkType: "quiz" | "results";
 }
-const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
+const LinkQuiz = ({ slug, setCopiedObj, linkType }: Props) => {
   const [referenceElement, setReferenceElement] =
     useState<HTMLDivElement | null>(null);
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
     null
   );
+
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
     placement: "top",
     modifiers: [
@@ -32,8 +32,8 @@ const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
       },
     ],
   });
-  const [showPopper, setShowPopper] = useState(false);
 
+  const [showPopper, setShowPopper] = useState(false);
   const [clicked, setClicked] = useState(false);
 
   const copyToClipboard = (str: string) => {
@@ -41,6 +41,7 @@ const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
   };
 
   useEffect(() => {
+    // resetting clicked state to false
     if (clicked) {
       const timer = setTimeout(() => {
         setClicked(false);
@@ -59,15 +60,17 @@ const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
   }, [clicked]);
 
   return (
-    // <Tooltip ref={copyRef} text="Copy to clipboard" setCopiedObj={setCopiedObj}>
     <>
       <div
         role="button"
         className={clsx(
           "m-auto my-2 flex w-80 max-w-full cursor-pointer items-center justify-between rounded border border-[#5c6070] bg-[#40434f] p-1"
         )}
-        ref={setReferenceElement}
-        onClick={() => copyToClipboard(slug)}
+        ref={(el) => setReferenceElement(el)}
+        onClick={() => {
+          copyToClipboard(slug);
+          setClicked(true);
+        }}
         onMouseEnter={(e) => {
           setReferenceElement(e.currentTarget);
           setShowPopper(true);
@@ -89,7 +92,7 @@ const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
         <p className="h-8 w-full cursor-pointer truncate bg-[#40434f] pr-1 text-white focus:outline-none">{`${window.location.origin}/quiz/${slug}`}</p>
         <div className="flex h-8 w-7 items-center justify-center">
           <FadeInFadeOut
-            isOpen={isOpen}
+            isOpen={clicked}
             falseChild={<CopyIcon fill="white" />}
             trueChild={<CheckIcon fill="white" />}
           />
@@ -100,41 +103,33 @@ const LinkQuiz = ({ slug, isOpen, setCopiedObj, linkType }: Props) => {
           <div
             ref={setPopperElement}
             style={styles.popper}
+            className="bottom-0 flex w-auto items-end justify-center"
+            role="tooltip"
             {...attributes.popper}
           >
-            <div className="flex h-8 w-auto items-center justify-center">
-              <FadeInFadeOut
-                isOpen={clicked}
-                falseChild={
-                  <div className="mb-1 flex w-auto flex-col" role="tooltip">
-                    <div className="shadow-top-sm z-50 w-[150px] max-w-full rounded bg-[#40434f] p-1.5">
-                      <p className="relative text-center text-white">
-                        {/* {text} */}
-                        hey
-                      </p>
-                    </div>
-                    <div className="z-40 m-auto -mt-1.5 h-3 w-3 rotate-45 bg-[#40434f]" />
+            <FadeInFadeOut
+              isOpen={clicked}
+              falseChild={
+                <>
+                  <div className="shadow-top-sm z-50 w-[150px] max-w-full rounded bg-[#40434f] p-1.5">
+                    <p className="text-center text-white">Copy to clipboard!</p>
                   </div>
-                }
-                trueChild={
-                  <div className="mb-1 flex w-auto flex-col" role="tooltip">
-                    <div className="shadow-top-sm z-50 w-[150px] max-w-full rounded bg-[#40434f] p-1.5">
-                      <p className="relative text-center text-white">
-                        {/* {clickedText} */}
-                        LOOOOOOOOL
-                      </p>
-                    </div>
-                    <div className="z-40 m-auto -mt-1.5 h-3 w-3 rotate-45 bg-[#40434f]" />
+                  <div className="z-40 m-auto -mt-1.5 h-3 w-3 rotate-45 bg-[#40434f]" />
+                </>
+              }
+              trueChild={
+                <>
+                  <div className="shadow-top-sm z-50 w-[150px] max-w-full rounded bg-[#40434f] p-1.5">
+                    <p className="text-center text-white">Copied!</p>
                   </div>
-                }
-              />
-            </div>
+                  <div className="z-40 m-auto -mt-1.5 h-3 w-3 rotate-45 bg-[#40434f]" />
+                </>
+              }
+            />
           </div>
         )}
       </Portal>
     </>
-
-    // </Tooltip>
   );
 };
 export default LinkQuiz;
